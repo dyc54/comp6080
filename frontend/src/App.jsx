1735a61d94069004d47c2731b7f8efabc6173676
ZHAO ZHANG <z5355933@ad.unsw.edu.au>
2022-04-12 01:31:11 +1000

    “create”

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './App.css';
+import { Title } from './style';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 
 function App () {
   return (
-    <></>
+    <>
+      <BrowserRouter>
+        <nav><Title>
+          <Link to='/register'>
+            Register
+          </Link>| |
+          <Link to='/login'>
+            Login
+          </Link>
+        </Title></nav>
+        <Routes>
+          <Route path='/register' element = {<Register />}/>
+          <Route path='/dashboard' element = {<Dashboard />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
