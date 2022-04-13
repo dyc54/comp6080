26ce4e76786ed646706607760d504c6f71c7fba7
ZHAO ZHANG <z5355933@ad.unsw.edu.au>
2022-04-14 01:18:20 +1000

    finfish dashboard function and style

--- frontend/src/components/DashboardForm.jsx
+++ frontend/src/components/DashboardForm.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, Box } from '@material-ui/core';
 import { useNavigate } from 'react-router-dom';
+import GetquizsForm from './GetquizsForm'
 
 function DashboardForm () {
   const token = localStorage.getItem('token');
   const navigate = useNavigate();
+  const [name, setGameName] = React.useState('');
+
   React.useEffect(() => {
     if (token === 'undefined') {
       navigate('/Register');
@@ -12,19 +15,26 @@ function DashboardForm () {
     }
   });
 
-  React.useEffect(() => {
-    fetch('http://localhost:5005/admin/quiz', {
-      method: 'GET',
+  const createGame = async () => {
+    await fetch('http://localhost:5005/admin/quiz/new', {
+      method: 'POST',
       headers: {
+        'Content-type': 'application/json',
         Authorization: `Bearer ${token}`,
-      }
-    }).then(response => response.json())
-      .then(data => console.log(data.quizzes));
-  });
+      },
+      body: JSON.stringify({ name }),
+    });
+  };
 
   return (
     <>
-      <Button variant='contained' color='primary'>Dashboard</Button>
+    <Box p = {1}>
+      <a>Create a new game: </a>
+      <input type = 'text' placeholder='Name' onChange={e => setGameName(e.target.value)}></input>
+      <a> </a>
+      <Button variant='contained' color='primary' onClick={createGame}>Create</Button>
+    </Box>
+    <GetquizsForm />
     </>
   )
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import DashboardForm from '../components/DashboardForm'
 import NavForm from '../components/NavForm'
+import { Title } from '../style';
+import { Box, Paper, Grid, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+  },
+}));
 
 function Dashboard () {
+  const classes = useStyles();
   return <>
       <NavForm />
-      <h1>Dashboard</h1>
-      <DashboardForm />
+      <div className={classes.root}>
+      <Grid item xs={12}>
+        <Box color = 'primary'>
+            <Paper variant = 'outlined'>
+                <Title>Dashboard
+                </Title>
+                <DashboardForm />
+            </Paper>
+        </Box>
+      </Grid>
+  </div>
   </>;
 }
 
