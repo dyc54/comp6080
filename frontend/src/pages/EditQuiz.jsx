e4b911e428839ebd313b13edd97c76c80891a753
Glenn Deng <z5362100@ad.unsw.edu.au>
2022-04-14 15:42:18 +1000

    partially done edit

---
+++ frontend/src/pages/EditQuiz.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import EditQuizForm from '../components/EditQuizForm'
+import { Title } from '../style';
+import { Box, Paper, Grid, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+  },
+}));
+
+function QuizEdit () {
+  const classes = useStyles();
+  return <>
+  <div className={classes.root}>
+      <Grid item xs={12}>
+        <Box color = 'primary'>
+            <Paper variant = 'outlined'>
+                <Title>Edit Game
+                </Title>
+                <EditQuizForm />
+            </Paper>
+        </Box>
+      </Grid>
+  </div>
+  </>;
+}
+
+export default QuizEdit;
